917b1d48f11e4fc402c6eaae700377a91d3e9752
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-10-19 13:36:31 +0300

    first commit

---
+++ SQlite tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect('/home/sasha/test.db')
+cur = conn.cursor()
+
+
+def get_users():
+    with conn:
+        cur.execute("SELECT * FROM users")
+        rows = cur.fetchall()
+        for row in rows:
+            print (row)
+
+
+get_users()
