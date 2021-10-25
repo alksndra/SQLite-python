b6c12ec7f204495f489869aeb91f100cdd267de6
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-10-25 10:01:47 +0300

    print users

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ conn = sqlite3.connect('test.db')
 cur = conn.cursor()
 
 
-def print_users():
-    cur.execute("SELECT * FROM users")
-    rows = cur.fetchall()
-    for row in rows:
-        print (row)
-
-
-print_users()
-
-
 class Users:
 
     def __init__(self, id, name):
         self.id = id
         self.name = name
+
+
+def print_users():
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    for row in rows:
+        user = Users(row[0], row[1])
+        print (user.id, user.name)
+
+
+print_users()
 
 
 test_user = Users(1, "David")
